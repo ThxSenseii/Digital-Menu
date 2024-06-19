d43efd89deef70434e3d13af5ff0bed2141d3eae
Ernesto Echeverría González <ernestoechegon@gmail.com>
2024-06-19 15:11:33 +0100

    findone user

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -4,14 +4,10 @@ const User = require('../models/user.model');
 
 function login(req, res) {
   const { user, password } = req.body;
-  User.findOne({ user, password }, (err, user) => {
-    if (err) {
-      return res.status(500).send({ message: err.message });
-    }
-    if (!user) {
-      return res.status(404).send({ message: 'User not found' });
-    }
+  User.findOne({ user, password }).then(user => {
     res.status(200).send({ token: createToken(user) });
+  }).catch(err => {
+    return res.status(500).send({ message: err.message });
   });
 }
 
